6b92202fcc136cf6fb708eeda36b340e73f91bdb
Oluwayelu <oluwayeluifeoluwa@gmail.com>
2020-03-25 13:08:30 +0100

    Day1 30 days of code

--- 30_Days_of_Code/Day1/index.js
+++ 30_Days_of_Code/Day1/index.js
@@ -4,7 +4,7 @@ Day 1 of 30 Days of Code
 Write a program to check from two given numbers, whether one is positive and another one is negative
 
 
-Created on Wed Mar 24 01:06:40 2020
+Created on Wed Mar 25 01:06:40 2020
 
 @author: Oluwayelu Ifeoluwa
 */
