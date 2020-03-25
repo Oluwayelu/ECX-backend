aa70d24ef13d05b13a05b9acecbe0fd1778f3d9e
Oluwayelu <oluwayeluifeoluwa@gmail.com>
2020-03-25 13:01:28 +0100

    Day0 30 days of Code

--- 30_Days_of_Code/Day1/index.js
+++ 30_Days_of_Code/Day1/index.js
@@ -1,20 +1,7 @@
-/*
-Day 1 of 30 Days of Code
 
-Write a program to display the current day and time in the following format.
-Sample Output: 
-Today is : Tuesday.
-Current time is: 10PM:30:38
+const checkNumber = num => {
+    num >= 0 ? console.log('Positive') : console.log('Negative')
+}
 
-
-Created on Tue Mar 24 04:45:20 2020
-
-@author: Oluwayeu Ifeoluwa
-*/
-const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-let hours 
-
-new Date().getSeconds() > 12 ? hours = `${new Date().getHours() - 12}PM` : hours = `${new Date().getHours() - 12}AM`
-
-console.log(`Today is: ${days[new Date().getDay() - 1]}`)
-console.log(`Current time is : ${hours}:${new Date().getMinutes()}:${new Date().getSeconds()} `)
+// Test
+checkNumber(-6)
