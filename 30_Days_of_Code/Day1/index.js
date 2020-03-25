98f3f82b5e1d532bb7200ba489d144ecba15cfda
Oluwayelu <oluwayeluifeoluwa@gmail.com>
2020-03-25 13:03:24 +0100

    Day1 30 days of code

--- 30_Days_of_Code/Day1/index.js
+++ 30_Days_of_Code/Day1/index.js
@@ -1,7 +1,14 @@
+/*
+Day 1 of 30 Days of Code
+
+Write a program to check from two given numbers, whether one is positive and another one is negative
+
+
+Created on Wed Mar 24 04:45:20 2020
+
+@author: Oluwayelu Ifeoluwa
+*/
 
 const checkNumber = num => {
     num >= 0 ? console.log('Positive') : console.log('Negative')
 }
-
-// Test
-checkNumber(-6)
