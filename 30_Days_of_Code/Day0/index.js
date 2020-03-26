b341d83c8b66605d2fe5aefab1daf9f6c142d7c3
Ifeoluwa Oluwayelu <46842207+Oluwayelu@users.noreply.github.com>
2020-03-26 10:18:28 +0100

    update index.js

--- 30_Days_of_Code/Day0/index.js
+++ 30_Days_of_Code/Day0/index.js
@@ -14,7 +14,7 @@ Created on Tue Mar 24 04:45:20 2020
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 let hours 
 
-new Date().getSeconds() > 12 ? hours = `${new Date().getHours() - 12}PM` : hours = `${new Date().getHours() - 12}AM`
+new Date().getSeconds() > 12 ? hours = `${new Date().getHours() - 12}PM` : hours = `${new Date().getHours()}AM`
 
 console.log(`Today is: ${days[new Date().getDay() - 1]}`)
 console.log(`Current time is : ${hours}:${new Date().getMinutes()}:${new Date().getSeconds()} `)
