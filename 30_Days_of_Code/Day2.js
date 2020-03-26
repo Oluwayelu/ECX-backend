6bc50852eb59e56e1d45ac59d1b53db6f7df9cbe
Ifeoluwa Oluwayelu <46842207+Oluwayelu@users.noreply.github.com>
2020-03-26 12:47:11 +0100

    Day2 ECX 30 days of code

---
+++ 30_Days_of_Code/Day2.js
@@ -0,0 +1,18 @@
+/*
+
+Day 2 of 30 Days of Code
+
+Write a JavaScript function to validate whether an argument of a given value type is error or not.
+
+Created on Thur Mar 26 12:45:36pm 2020
+
+@author: Oluwayelu Ifeoluwa
+
+*/
+
+const validate = (value, valueType) => {
+  typeof(value) !== valueType ? console.log("Error") : console.log("No error")
+}
+
+//test
+validate("ECX", "string")
