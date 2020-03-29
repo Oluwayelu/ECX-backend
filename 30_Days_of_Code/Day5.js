909e39f7c30f87c956abd5a6d53ad918ee42c0ce
Ifeoluwa Oluwayelu <46842207+Oluwayelu@users.noreply.github.com>
2020-03-29 17:17:39 +0100

    Update Day5.js

--- 30_Days_of_Code/Day5.js
+++ 30_Days_of_Code/Day5.js
@@ -4,15 +4,17 @@ Day 5 of 30 Days of Code
 
 Write a JavaScript function that checks whether a passed string is palindrome or not?
 
-Created on Sun Mar 29 2:23:48pm 2020
+Created on Sun Mar 29 5:10:08pm 2020
 
 @author: Oluwayelu Ifeoluwa
 */
 
 const noReverse = value => {
+  var re = /[\W_]/g;
+  value = value.toLowerCase().replace(re, "")
   
   value === value.split("").reverse().join("") ? console.log(true) : console.log(false)
 }
 
 //test
-noReverse("aba")
+noReverse("A man, a plan, a canal. Panama")
