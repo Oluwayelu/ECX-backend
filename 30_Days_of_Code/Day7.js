2cf885cdc5c11ccede0b0e40b262098b36fa8f45
Ifeoluwa Oluwayelu <46842207+Oluwayelu@users.noreply.github.com>
2020-03-31 17:26:02 +0100

    Update Day7.js

--- 30_Days_of_Code/Day7.js
+++ 30_Days_of_Code/Day7.js
@@ -6,16 +6,28 @@ Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . Each s
 number is the sum of the previous two.
 
 Created on Tue Mar 31 3:56:12pm 2020
+Updated on Tue Mar 31 5:23:34pm 2020
 
 @author: Oluwayelu Ifeoluwa
 */
-const donFibonacci = n => {
+  
+//Solved with Iteration
+const donFibIterative = n => {
   let ar = [0, 1]
-  for(let i = 0; i<=n; i++){
+  for(let i = 2; i<=n; i++){
     ar.push(ar[ar.length-1] + ar[ar.length-2])
   }
   return ar[n]
 }
 
+//Solved with Recursion
+const donFibRecursive = n => {
+  if (n < 2) {
+    return n
+  }
+  return donFibRecursive(n-1) + donFibRecursive(n-2)
+}
+
 //test
-console.log(donFibonacci(7)) //13
+console.log(donFibIterative(7)) //13
+console.log(donFibRecursive(7)) //13
