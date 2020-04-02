e8782251e7baccf796fb0617765ff529dfb04e57
Ifeoluwa Oluwayelu <46842207+Oluwayelu@users.noreply.github.com>
2020-04-02 09:33:58 +0100

    ECX Day9 30 days of code

---
+++ 30_Days_of_Code/Day9.js
@@ -0,0 +1,22 @@
+
+/*
+Day 9 of 30 Days of Code
+
+Write a class method to return the number of tyres of a Car Class
+
+Created on Thur Apr 2 9:21:47am 2020
+
+@author: Oluwayelu Ifeoluwa
+*/
+
+class Car{
+  
+  autoBahn(){
+    this.tyre = 4
+    return this.tyre
+  }
+}
+
+//test
+car = new Car()
+console.log(car.autoBahn())
