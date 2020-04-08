21ffe5a2a3592e7fddca32109b94b9feb24778d2
Ifeoluwa Oluwayelu <46842207+Oluwayelu@users.noreply.github.com>
2020-04-08 22:10:13 +0100

    Update Index.js

--- 30_Days_of_Code/Day15/index.js
+++ 30_Days_of_Code/Day15/index.js
@@ -7,15 +7,20 @@ const port = process.env.PORT || 8000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-
-//Routes
 let sgpa
+
+// @route   POST /createdata
+// @desc    calculates the sgpa of the courses sent to the server
+// @access  Public
 app.post('/createdata', (req, res) => {
 
     sgpa = sgpaCalc(req.body.courses)    
     res.status(200).json({ msg: 'Data sent' })
 })
 
+// @route   GET /getdata
+// @desc    sends the sgpa to the user
+// @access  Public
 app.get('/getdata', (req, res) => {
     res.status(200).json({ sgpa: sgpa })
 })
