2d5b4cb16245e003cbe0a9d920b66aaf82075c80
Oluwayelu <oluwayeluifeoluwa@gmail.com>
2020-04-09 22:53:20 +0100

    ECX Day16 30 days of code

--- 30_Days_of_Code/Day16/index.js
+++ 30_Days_of_Code/Day16/index.js
@@ -1,3 +1,13 @@
+/*
+Day 16 of 30 Days of Code
+
+Write a program to check from two given numbers, whether one is positive and another one is negative
+
+Created on Thur Apr 9 10:48:33pm 2020
+
+@author: Oluwayelu Ifeoluwa
+*/
+
 const express = require('express')
 const bcrypt = require('bcrypt')
 
@@ -9,8 +19,10 @@ const port = process.env.PORT || 8000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-//database
-let user 
+
+//Database
+let user
+
 
 // @route   POST /signup
 // @desc    Register user
@@ -18,12 +30,12 @@ let user
 app.post('/signup', (req, res) => {
     const { email, username, password } = req.body
 
-    (!emailValidator(email)) ?
+    if(!emailValidator(email)) {
         res.status(400).json({ success: false, msg: 'Invalid email'})  
-    :
-        (!passwordValidator(password)) ?
+    } else {
+        if(!passwordValidator(password)) {
             res.status(400).json({ success: false, msg: 'Password should be at least 6 charcters long' })
-        :
+        } else {
             user = {
                 email,
                 username,
@@ -32,29 +44,38 @@ app.post('/signup', (req, res) => {
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(user.password, salt, (err, hash) => {
                     user.password = hash
-                    res.status(200).json({ success: true, msg: 'User registered' })
+                    res.status(200).json({ success: true, msg: 'User registered', user })
                 })
             })
+        }
+    }
 })
 
-// @route   GET /login
+// @route   POST /login
 // @desc    Login User
 // @access  Public
 app.post('/login', (req, res) => {
     const { email, password } = req.body
 
-    (!emailValidator(email) || !passwordValidator(password)) ?
-        res.status(400).json({ success: false, msg: 'Invalid Credentials'})  
-    : 
-        (email !== user.email) ?
-            res.status(400).json({ success: false, msg: 'Email does not exist' })
-        :
-            bcrypt.compare(password, user.password, (err, isMatch) => {
-                (isMatch) ?
-                    res.status(200).json({ success: true, msg: 'Login Successful' })
-                :
-                    res.status(400).json({ success: false, msg: 'Incorrect password' })
-            })
+    if(!emailValidator(email)) {
+        res.status(400).json({ success: false, msg: 'Invalid email'})  
+    } else {
+        if(!passwordValidator(password)) {
+            res.status(400).json({ success: false, msg: 'Password should be at least 6 charcters long' })
+        } else {
+            if(email === user.email) {
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if(isMatch) {
+                        res.status(200).json({ success: true, email: user.email, username: user.username })
+                    } else {
+                        res.status(400).json({ success: false, msg: 'Invalid Credentials'})
+                    }
+                })
+            } else {
+                res.status(400).json({ success: false, msg: 'Invalid Credentials' })
+            }
+        }
+    }
 })
 
 app.listen(port, () => {
